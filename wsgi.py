e2811421b68a54ba0820e63630926ccf3d470422
Gabrielle Seepersad <gabrielleseepersad@gmail.com>
2025-10-01 23:22:10 -0400

    final commit

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,12 @@
 import click, pytest, sys
+#from flask import Flask
 from flask.cli import with_appcontext, AppGroup
 from datetime import datetime
 
 from App.database import db, get_migrate
 from App.models import User, Staff, Admin, Roster, TimeLog, Shift
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
+from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize,  )
 
 
 # This commands file allow you to create convenient CLI commands for testing controllers
@@ -47,31 +48,24 @@ def init():
 User Commands
 '''
 
-# Commands can be organized using groups
+# User Commands
 
-# create a group, it would be the first argument of the comand
-# eg : flask user <command>
-user_cli = AppGroup('user', help='User object commands') 
-
-# Then define the command and any parameters and annotate it with the group (@)
-@user_cli.command("create", help="Creates a user")
+#command to create a user
+@app.cli.command("create", help="Creates a user")
 @click.argument("username", default="rob")
 @click.argument("password", default="robpass")
 def create_user_command(username, password):
     create_user(username, password)
     print(f'{username} created!')
 
-# this command will be : flask user create bob bobpass
-
-@user_cli.command("list", help="Lists users in the database")
+#command to list users
+@app.cli.command("list", help="Lists users in the database")
 @click.argument("format", default="string")
 def list_user_command(format):
     if format == 'string':
         print(get_all_users())
     else:
         print(get_all_users_json())
-
-app.cli.add_command(user_cli) # add the group to the cli
 
 '''Added Commands'''
 #command to schedule staff
