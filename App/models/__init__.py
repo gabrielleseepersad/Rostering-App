13ff2eaf3b868abe9a24d43a1ca7bf73ce1bd9ac
gabrielleseepersad <gabrielleseepersad@gmail.com>
2025-10-01 23:39:01 -0400

    Update __init__.py

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,14 +1,6 @@
 from .user import *
-<<<<<<< HEAD
 from .staff import *
 from .admin import *
 from .roster import *
 from .shift import *
 from .timeLog import *
-=======
-from .staff import*
-from .roster import*
-from .shift import*
-from .timeLog import*
-from .admin import*
->>>>>>> 3e8b13d3f8813c2b1f358000033cd4a82234613c
